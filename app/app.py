4a61d7798ca6456945075b3797d9e3c86b900a67
Dave Stanke <davidstanke@gmail.com>
2022-05-04 01:29:15 +0000

    add special input handling

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import os
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 
 app = Flask(__name__)
 
@@ -17,6 +17,14 @@ def define(word):
     return render_template('index.html', word_input=word, definition=definition, app_header=app_header)
 
 def get_definition(word):
+    
+    # special case the word "error" to demonstrate error logging
+    if word == 'error':
+        abort(500,"You entered the word 'error,' so here's an error")
+    
+    if not word:
+        return '(no word entered)'
+    
     dictionary_api = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}')
     dictionary_api.raise_for_status()
     api_return = dictionary_api.json()
